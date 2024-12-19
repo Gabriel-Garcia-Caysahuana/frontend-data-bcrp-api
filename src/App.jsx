748264387677a7190a1c64ed836f4e24752c6fc1
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-12-19 13:51:05 -0500

    Preparación para despliege en git hub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import ApiForm from "./components/ApiForm";
 import Footer from "./components/Footer";
+import About from "./components/About"; // Importar el componente About
+import Questions from "./components/Questions";
 
 function App() {
   return (
-    <div className="App">
-      <ApiForm />
-      <Footer /> 
-    </div>
+    <Router>
+      <div className="App">
+        {/* Barra de navegación */}
+        <Navbar />
+        {/* Definición de rutas */}
+        <Routes>
+          <Route path="/" element={<ApiForm />} /> {/* Ruta principal */}
+          <Route path="/about" element={<About />} /> {/* Ruta "Acerca de" */}
+          <Route path="/questions" element={<Questions />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
