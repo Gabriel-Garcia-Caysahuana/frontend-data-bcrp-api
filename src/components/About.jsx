3c6289e0e279bd11f5e07a7c1f08922e3213d757
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-12-19 13:49:15 -0500

    Agregar componente

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => (
+    <div style={{ padding: "20px", backgroundColor: "#f9f9f9" }}>
+        <h2>Acerca de</h2>
+        <p>
+            Esta aplicación está diseñada para realizar análisis iniciales de series temporales de manera sencilla y gratuita.
+            Fue desarrollada con fines educativos e investigativos, utilizando datos públicos proporcionados por la API del Banco Central de Reserva del Perú (BCRP).
+        </p>
+        <p>
+            Si tienes comentarios o sugerencias, ¡no dudes en compartirlos!
+            Estamos comprometidos con mejorar la herramienta para que sea más útil y accesible.
+        </p>
+        <p>
+            Fuente de datos: <a href="https://estadisticas.bcrp.gob.pe/" target="_blank" rel="noopener noreferrer">Banco Central de Reserva del Perú</a>
+        </p>
+    </div>
+);
+export default About;
