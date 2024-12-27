14dbb92647d4d7fe0f84ea9059d862f83b29667c
Gabriel Garcia <garciacaysahuana@gmail.com>
2024-12-27 09:28:09 -0500

    Corregir error

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
-import axios, { create } from "axios";
+import axios from "axios";
 
-const api = create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
